d210eb1571201e95c66a331b54bad59349aa680f
spencer-eagleton <86867967+spencer-eagleton@users.noreply.github.com>
2021-11-30 16:42:54 -0800

    animal farm

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,6 +1,15 @@
 import React from 'react'
 import './Main.css'
+import background from '../../background.png'
+import { animals } from '../../data'
+import Animal from '../Animal/Animal'
 
 export default function Main() {
-  return <main></main>
+  return (
+    <main style={{ backgroundImage: `url(${background})` }}>
+      {animals.map((animal) => (
+        <Animal key={animal.type} {...animal} />
+      ))}
+    </main>
+  )
 }
